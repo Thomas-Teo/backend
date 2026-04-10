b9fcff8dd62d756907fd7f1ecec46115e66bf7bc
Thomas-Teo <thomasendersom@gmail.com>
2026-04-10 10:21:44 -0300

    feat(veiculo): criar entidade veiculo com campos basicos e iniciar repository

--- src/main/java/com/concessionaria/entity/Veiculo.java
+++ src/main/java/com/concessionaria/entity/Veiculo.java
@@ -1,4 +1,26 @@
 package com.concessionaria.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Veiculo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String modelo;
+    private String marca;
+    private String placa;
+    private int ano;
+    private Double valor;
 }
--- src/main/java/com/concessionaria/repository/VeiculoRepository.java
+++ src/main/java/com/concessionaria/repository/VeiculoRepository.java
@@ -1,4 +1,9 @@
 package com.concessionaria.repository;
 
-public interface VeiculoRepository {
+import com.concessionaria.entity.Veiculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
 }
